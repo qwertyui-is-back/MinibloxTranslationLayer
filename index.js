43f94848c65cc0ce71fc1b8844a6877f0fad9f53
qwertyui <watchdogbestac@gmail.com>
2025-01-07 05:13:32 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const { ClientSocket, SPacketLoginStart } = require('./miniblox/main.js');
+const http = require('http');
+const ngrok = require('@ngrok/ngrok');
+
+// Create webserver
+http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end('Congrats you have created an ngrok web server');
+}).listen(8080, () => console.log('Node.js web server at 8080 is running...'));
+
+// Get your endpoint online
+ngrok.connect({ addr: 8080, '2lUYcgF9YvUS5OB9rdtSzgtKFbM_7sywAJ1tzj1FjvESACGcY': true })
+	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
+
 const handlers = require('./miniblox/handlers/init.js');
 const mc = require('minecraft-protocol');
 const fs = require('node:fs');
